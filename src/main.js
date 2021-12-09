4b798362672d81ba38a06ef2953e2cf9a4796779
Ansgar Lichter <lichteransgar@gmail.com>
2021-12-09 15:01:03 +0100

    registerd font awesome icons

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import i18n from './i18n';
+import FontAwesomeIcon from './fontAwesomeIcons';
 
 import './index.css';
 
 
 const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(i18n);
 
