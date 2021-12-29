f3fea90d78c76140653c9180b2f59f2978200937
Ansgar Lichter <lichteransgar@gmail.com>
2021-12-29 14:30:02 +0100

    Improve jsdocs

--- src/scrollspy/util/MergeOptions.js
+++ src/scrollspy/util/MergeOptions.js
@@ -1,20 +1,20 @@
 /**
  * Merges the default options and the bound options.
- * The default options are used as a fallback.
+ * The default options are used as the template.
  * This means all properties from the default options are set.
- * If they are set in the bound options as well the value from the bound options will be used.
+ * If the options from the binding have the same properties these values will be set.
  *
  * @param {object} defaultOptions default options which are used as a fallback
- * @param {object} options bound options
+ * @param {object} boundOptions options from binding
  *
  * @returns  {object} merged options
  */
-const mergeOptions = (defaultOptions, options) => {
+const mergeOptions = (defaultOptions, boundOptions) => {
   const mergedOptions = defaultOptions;
 
   for (const propertyName in mergedOptions) {
-    if (options && Object.prototype.hasOwnProperty.call(options, propertyName)) {
-      mergedOptions[propertyName] = options[propertyName];
+    if (boundOptions && Object.prototype.hasOwnProperty.call(boundOptions, propertyName)) {
+      mergedOptions[propertyName] = boundOptions[propertyName];
     }
   }
 
