6caec1f10a94e3803569e50525dc3b58c97f1966
Ansgar Lichter <lichteransgar@gmail.com>
2022-03-01 20:06:09 +0100

    [ADD] animated other projects (#31)

--- src/views/OtherProjects.vue
+++ src/views/OtherProjects.vue
@@ -1,56 +1,88 @@
 <template>
   <div class="bg-primaryLight py-8 px-8 sm:pt-24 sm:px-24 lg:py-16 lg:px-4">
-    <h1 class="text-2xl sm:text-4xl text-secondaryLight font-bold mb-8 lg:text-5xl">
-      {{ t("otherProjects.title") }}
-    </h1>
-    <div class="flex flex-col lg:flex-row lg:justify-center mb-8 last:mb-0 lg:space-x-20">
-      <div
+    <UseElementIsVisible abortIfVisible="true" v-slot="{ isVisible }">
+      <FadeInUp>
+        <h1
+          v-visible="isVisible"
+          class="
+            text-2xl
+            sm:text-4xl
+            text-secondaryLight
+            font-bold
+            mb-8
+            lg:text-5xl
+          "
+        >
+          {{ t("otherProjects.title") }}
+        </h1>
+      </FadeInUp>
+    </UseElementIsVisible>
+    <div
+      class="
+        flex flex-col
+        lg:flex-row lg:justify-center
+        mb-8
+        last:mb-0
+        lg:space-x-20
+      "
+    >
+      <UseElementIsVisible
+        abortIfVisible="true"
+        v-slot="{ isVisible }"
         v-for="(project, index) of otherProjects"
         :key="index"
-        class="shadow-xl rounded-md p-5 bg-primary mb-8 lg:mb-0 lg:w-144"
       >
-        <div class="flex flex-row items-center space-x-4">
-          <font-awesome-icon
-            class="text-5xl mr-auto"
-            :icon="['far', 'folder']"
-            color="#007fad"
-          />
-          <ProjectIcon
-            v-if="project.links.googlePlay"
-            :href="project.links.googlePlay.href"
-            :linkDescription="project.links.github.description"
-            :icon="icons.googlePlay"
-            iconColor="#007fad"
-          />
-          <ProjectIcon
-            v-if="project.links.github"
-            :href="project.links.github.href"
-            :linkDescription="project.links.description"
-            :icon="icons.github"
-            iconColor="#007fad"
-          />
-          <ProjectIcon
-            v-if="project.links.external"
-            :href="project.links.external.href"
-            :linkDescription="project.links.external.description"
-            :icon="icons.external"
-            iconColor="#007fad"
-          />
-        </div>
-        <div class="mt-4 text-2xl sm:text-4xl text-secondaryLight">
-          {{ project.title }}
-        </div>
-        <div class="mt-4 sm:mt-8 text-secondary">{{ project.description }}</div>
-        <div class="mt-4 sm:mt-8 flex flex-row space-x-4 justify-center">
+        <FadeInUp>
           <div
-            class="flex flex-row text-accent"
-            v-for="(technology, index) of project.technologies"
-            :key="index"
+            v-visible="isVisible"
+            class="shadow-xl rounded-md p-5 bg-primary mb-8 lg:mb-0 lg:w-144"
           >
-            {{ technology }}
+            <div class="flex flex-row items-center space-x-4">
+              <font-awesome-icon
+                class="text-5xl mr-auto"
+                :icon="['far', 'folder']"
+                color="#007fad"
+              />
+              <ProjectIcon
+                v-if="project.links.googlePlay"
+                :href="project.links.googlePlay.href"
+                :linkDescription="project.links.github.description"
+                :icon="icons.googlePlay"
+                iconColor="#007fad"
+              />
+              <ProjectIcon
+                v-if="project.links.github"
+                :href="project.links.github.href"
+                :linkDescription="project.links.description"
+                :icon="icons.github"
+                iconColor="#007fad"
+              />
+              <ProjectIcon
+                v-if="project.links.external"
+                :href="project.links.external.href"
+                :linkDescription="project.links.external.description"
+                :icon="icons.external"
+                iconColor="#007fad"
+              />
+            </div>
+            <div class="mt-4 text-2xl sm:text-4xl text-secondaryLight">
+              {{ project.title }}
+            </div>
+            <div class="mt-4 sm:mt-8 text-secondary">
+              {{ project.description }}
+            </div>
+            <div class="mt-4 sm:mt-8 flex flex-row space-x-4 justify-center">
+              <div
+                class="flex flex-row text-accent"
+                v-for="(technology, index) of project.technologies"
+                :key="index"
+              >
+                {{ technology }}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </FadeInUp>
+      </UseElementIsVisible>
     </div>
   </div>
 </template>
@@ -60,6 +92,10 @@ import { computed } from "@vue/reactivity";
 import { useI18n } from "vue-i18n";
 
 import ProjectIcon from "@/components/ProjectIcon";
+
+import FadeInUp from "@/transitions/FadeInUp";
+
+import { UseElementIsVisible } from "@/composables";
 
 const { t } = useI18n();
 const otherProjects = computed(() => [
